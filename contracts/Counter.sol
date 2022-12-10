663745d833eb66632267f2f3597f9f52a7ece1a6
akshatcoder-hash <akshatcoder@outlook.com>
2022-12-10 18:18:47 +0530

    💄 Refactored Code w Syntactic Sugar

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.0;
 
 contract Counter {
-    uint count;
+    uint public count = 0;
 
-    constructor() public {
-        count = 0;
-    }
+    // constructor() public {
+    //     count = 0;
+    // }
 
-    function getCount() public view returns(uint) {
-        return count;
-    }
+    // function getCount() public view returns(uint) {
+    //     return count;
+    // }
 
     function incrementCount() public {
-        count = count + 1;
+        count++;
     }
 }
