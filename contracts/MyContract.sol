74c0716503cfaa14931bccb9325902f6bf4a4e34
akshatcoder-hash <akshatcoder@outlook.com>
2022-12-10 18:37:55 +0530

    Deployed Refactored Code

---
+++ contracts/MyContract.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    // State Variables
+    // Local Variables
+    function getValue() public {
+        uint value = 1;
+        return value;
+    }
+}
